73a85c212904105ace5b3f2d9d7e02a28bf48d27
Erica Roquemore <enroquemore@gmail.com>
2023-08-22 00:17:07 -0400

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// src/components/Header.jsx
+import React from 'react';
+import './Header.css'; 
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Erica Roquemore</h1>
+      <p>Frontend Developer</p>
+    </header>
+  );
+}
+
+export default Header;
