5f67a3874cad9edc72c597fa424baaccf6b73f03
Erica Roquemore <enroquemore@gmail.com>
2023-08-30 23:25:13 -0400

    fixing build errors

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // Automatically open in browser when server starts
+    open: process.env.NODE_ENV === 'development', // Only open in browser during development
   },
 });
