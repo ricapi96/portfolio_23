db8529b00f522b0ddf26fb6e7905c9e5c6143a39
Erica Roquemore <enroquemore@gmail.com>
2023-08-30 23:37:13 -0400

    deploy issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: process.env.NODE_ENV === 'development', // Only open in browser during development
-  },
-});
+    port: 3000,
+    open: true
+  }
+})
