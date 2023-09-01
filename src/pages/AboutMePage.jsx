469be781be4b4246052a2d61cdf000db9eed2b9a
Erica Roquemore <enroquemore@gmail.com>
2023-08-31 21:53:25 -0400

    logo and borders

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -12,20 +12,20 @@ function AboutMePage() {
       <p>
         Hi there! I'm Erica, a passionate frontend developer with a love for nature and animals. My journey began when I pursued a degree in computer science and engineering, where I gained a strong foundation in coding and problem-solving.
       </p>
-      <img src={bigCity} alt="Me with city background" className="big-city" />
+      <img src={bigCity} alt="Me with city background" className="big-city rounded-image" />
       <p>
         When I'm not crafting pixel-perfect interfaces, you can find me exploring the great outdoors, often accompanied by my furry friends. I love dogs and ducks! I believe that just as in coding, attention to detail and adaptability are essential when trekking new paths and discovering hidden gems.
       </p>
-      <img src={goosey} alt="Lady no kids" className="goose" />
+      <img src={goosey} alt="Lady no kids" className="goose rounded-image" />
       <p>
         In my downtime, I'm an avid traveler, seeking inspiration from diverse cultures and landscapes. When I'm not on the road, I'm likely immersed in the virtual worlds of video games, where creativity and strategy come together in exciting ways.
       </p>
-      <img src={tullip} alt="Me with a flower" className="flower" />
+      <img src={tullip} alt="Me with a flower" className="flower rounded-image" />
       <p></p>
       <p>
         I'm excited to combine my technical skills with my passion for design to create intuitive user experiences that make a positive impact. Let's build something amazing together!
       </p>
-      <img src={coke} alt="Me with a coke" className="coke" />
+      <img src={coke} alt="Me with a coke" className="coke rounded-image " />
     </section>
   );
 }
