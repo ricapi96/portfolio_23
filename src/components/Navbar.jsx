6850fb5531c21aa4f83129f0ec5eebe3e8a1b1ab
Erica Roquemore <enroquemore@gmail.com>
2023-09-11 21:55:10 -0400

    readme and refresh on home link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,32 @@
 // Navbar.jsx
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 function Navbar() {
+  const handleHomeClick = () => {
+    window.location.reload();
+  };
+
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li><RouterLink to="/">Home</RouterLink></li>
-        <li><RouterLink to="/about">About</RouterLink></li>
-        <li><RouterLink to="/projects">Projects</RouterLink></li>
-        <li><RouterLink to="/stacks">Stacks</RouterLink></li>
-        <li><RouterLink to="/contact">Contact</RouterLink></li>
+        <li>
+          <RouterLink to="/" onClick={handleHomeClick}>
+            Home
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/projects">Projects</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/stacks">Stacks</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/contact">Contact</RouterLink>
+        </li>
       </ul>
     </nav>
   );
