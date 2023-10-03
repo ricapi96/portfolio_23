8eff613e80e0a6166cca6d69a76e58c63f696161
Erica Roquemore <enroquemore@gmail.com>
2023-10-02 20:44:36 -0400

    modal

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,28 @@
 // Contact.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import coke from '/images/coke.png';
+// import coke from '/images/coke.png';
 import ContactForm from '../components/ContactForm';
 
 function ContactPage() {
+  const [showForm, setShowForm] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const openForm = () => {
+    console.log('Opening the form');
+    setShowForm(true);
+  };
+
+  const closeForm = () => {
+    setShowForm(false);
+  };
+
+  const handleSubmit = () => {
+    
+    setSubmitted(true);
+  };
+
   return (
     <section className="contact">
       <h2 id='contact' className='animate__animated animate__rubberBand'>Contact Me</h2>
@@ -19,22 +36,43 @@ function ContactPage() {
       <p>
         Let's work together to bring your ideas to life!
       </p>
-      
-      
-      <div className='pics'>
-    <img src={coke} alt="Me with a coke" className="coke rounded-image" />
-    <br />
-    <br />
-    <p>Feel free to send me a message!</p>
-      <ContactForm />
-      <br />
-      <br />
-      <br />
-    </div> 
-      
+
+      <button onClick={openForm}>Message Me</button>
+
+      {/* Message modal */}
+      {showForm && !submitted && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeForm}>&times;</span>
+            <ContactForm onSubmit={handleSubmit} />
+          </div>
+        </div>
+      )}
+
+      {submitted && (
+        <div className="success-message">
+          Thank you for your message! I'll get back to you soon.
+        </div>
+      )}
     </section>
-    
   );
 }
+      
+      
+    //   <div className='pics'>
+    // <img src={coke} alt="Me with a coke" className="coke rounded-image" />
+    // <br />
+    // <br />
+    // <p>Feel free to send me a message!</p>
+    //   <ContactForm />
+    //   <br />
+    //   <br />
+    //   <br />
+    // </div> 
+      
+    // </section>
+    
+//   );
+// }
 
 export default ContactPage;
